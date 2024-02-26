725d2208a4d5f02946a13334979ff8ddd9e73fb5
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-26 19:03:28 +0100

    with bootstrap cards

--- index.php
+++ index.php
@@ -9,58 +9,68 @@
 </head>
 
 <body>
+    <div class="container">
+        <h1 class="mt-5 mb-4">Lista degli Hotel</h1>
 
-    <h1>Lista degli Hotel</h1>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            <?php
+            $hotels = [
+                [
+                    'name' => 'Hotel Belvedere',
+                    'description' => 'Hotel Belvedere Descrizione',
+                    'parking' => true,
+                    'vote' => 4,
+                    'distance_to_center' => 10.4
+                ],
+                [
+                    'name' => 'Hotel Futuro',
+                    'description' => 'Hotel Futuro Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 2
+                ],
+                [
+                    'name' => 'Hotel Rivamare',
+                    'description' => 'Hotel Rivamare Descrizione',
+                    'parking' => false,
+                    'vote' => 1,
+                    'distance_to_center' => 1
+                ],
+                [
+                    'name' => 'Hotel Bellavista',
+                    'description' => 'Hotel Bellavista Descrizione',
+                    'parking' => false,
+                    'vote' => 5,
+                    'distance_to_center' => 5.5
+                ],
+                [
+                    'name' => 'Hotel Milano',
+                    'description' => 'Hotel Milano Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 50
+                ],
+            ];
 
-    <?php
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-    ];
+            foreach ($hotels as $hotel) {
+            ?>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $hotel['name']; ?></h5>
+                            <p class="card-text"><strong>Description:</strong> <?php echo $hotel['description']; ?></p>
+                            <p class="card-text"><strong>Parking:</strong> <?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></p>
+                            <p class="card-text"><strong>Vote:</strong> <?php echo $hotel['vote']; ?></p>
+                            <p class="card-text"><strong>Distance to Center:</strong> <?php echo $hotel['distance_to_center']; ?> km</p>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
 
-    // Itera attraverso gli hotel e stampa le informazioni
-    foreach ($hotels as $hotel) {
-        echo "<h2>{$hotel['name']}</h2>";
-        echo "<p><strong>Description:</strong> {$hotel['description']}</p>";
-        echo "<p><strong>Parking:</strong> " . ($hotel['parking'] ? 'Yes' : 'No') . "</p>";
-        echo "<p><strong>Vote:</strong> {$hotel['vote']}</p>";
-        echo "<p><strong>Distance to Center:</strong> {$hotel['distance_to_center']} km</p>";
-        echo "<hr>";
-    }
-    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
